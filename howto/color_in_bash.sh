7b3ea57d2517347d154f6e576c128d465f7f1f2a
Adrien Bresson <bresson.adrien@gmail.com>
2013-06-12 10:50:40 +0200

    update spreadsheets

---
+++ howto/color_in_bash.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+VERT="\e[1;32m"
+NORMAL="\e[0;39m"
+ROUGE="\e[1;31m"
+ROSE="\e[1;35m"
+BLEU="\e[1;34m"
+BLANC="\e[0;02m"
+BLANCLAIR="\e[1;08m"
+JAUNE="\e[1;33m"
+CYAN="\e[1;36m"
+
+echo    ""
+echo -e "$VERT" "    ------------------ ""$ROUGE""Y o u  W i l l  B e  C o l o r e d ""$VERT""  ------------    " "$NORMAL"
+echo            "    |                     Color bash  script                             |  "
+echo -e         "    |                 written by""$VERT" "selim,b." "$CYAN""(s.bouras@free.fr)  "      "$NORMAL"       "         |  "
+echo -e         "    |                    This script is""$BLEU"" free Licence""$NORMAL""                     |  "
+echo -e "$VERT" "    --------------------------------------------------------------------     " "$NORMAL"
+echo -e "                     $BLEU" " Ok!!..Dear ... Your bash is colored .. Have Fun!!..$NORMAL"
