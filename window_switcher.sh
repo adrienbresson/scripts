b2e8e67d446e5105206f489ad107211fa7571279
Adrien Bresson <abresson@kaliop.com>
2014-04-30 11:28:35 +0200

    update script

--- window_switcher.sh
+++ window_switcher.sh
@@ -4,7 +4,7 @@
 current_worspace=$(wmctrl -d |awk '{print $1" "$2}'  |grep -F '*' |awk '{print $1}')
 
 if [[ $1 == 'all' ]]; then 
-     wmctrl -l |sort -k4 |sort -k2 |awk '{printf "%s %s ", $2,$1;s = ""; for (i = 4; i <= NF; i++) s = s $i " ";print s }' |grep -E '^[1-9]' |dmenu -l 20 |cut -d' ' -f 2 |xargs wmctrl -i -a
+     wmctrl -l |sort -k4 |sort -k2 |awk '{printf "%s %s ", $2,$1;s = ""; for (i = 4; i <= NF; i++) s = s $i " ";print s }' |grep -E '^[0-9]' |dmenu -l 20 |cut -d' ' -f 2 |xargs wmctrl -i -a
      exit 0
 fi
 
