2f6f1dff0fca834d34c1399aae715ff387e13100
Adrien Bresson <abresson@kaliop.com>
2014-04-30 11:23:45 +0200

    update window switcher

--- window_switcher.sh
+++ window_switcher.sh
@@ -3,4 +3,10 @@
 
 current_worspace=$(wmctrl -d |awk '{print $1" "$2}'  |grep -F '*' |awk '{print $1}')
 
+if [[ $1 == 'all' ]]; then 
+     wmctrl -l |sort -k4 |sort -k2 |awk '{printf "%s %s ", $2,$1;s = ""; for (i = 4; i <= NF; i++) s = s $i " ";print s }' |grep -E '^[1-9]' |dmenu -l 20 |cut -d' ' -f 2 |xargs wmctrl -i -a
+     exit 0
+fi
+
+
 wmctrl -l |grep -E "^[[:xdigit:]]+x[[:xdigit:]]+( )+${current_worspace}" |cut -d' ' -f 1,5- |dmenu -l 10 |cut -d' ' -f 1 |xargs wmctrl -i -a
